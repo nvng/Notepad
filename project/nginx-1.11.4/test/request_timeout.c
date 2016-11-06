cf87ee7699d8dd37e2fbab95150cd05e46776067
nvng <nvngliu@gmail.com>
2016-11-07 01:00:11 +0800

    a

--- project/nginx-1.11.4/test/request_timeout.c
+++ project/nginx-1.11.4/test/request_timeout.c
@@ -14,10 +14,20 @@
 #include <assert.h>
 
 // char req_header[] = "GET / HTTP/1.1\r\nUser-Agent:curl/7.19.7\r\nHost:127.0.0.1\r\nAccept: */*\r\n\r\n";
-char req_header[] = "GET / HTTP/1.1\r\nUser-Agent: curl/7.19.7\r\n";
+   char req_header[] = "GET / HTTP/1.1\r\nUser-Agent:curl/7.19.7\r\n";
 
 int main(int argc, char* argv[])
 {
+        if (3 != argc)
+        {
+                printf("Usage: %s ip port\n", argv[0]);
+                // ip   : 127.0.0.1
+                // port : 80
+                return -1;
+        }
+        const char* ip = argv[1];
+        uint16_t port = atoi(argv[2]);
+
         struct sockaddr_in server_addr;
 
         int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,9 +35,9 @@ int main(int argc, char* argv[])
 
         bzero(&server_addr, sizeof(server_addr));
         server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(80);
+        server_addr.sin_port = htons(port);
 
-        int ret = inet_aton("192.168.1.1", &server_addr.sin_addr);
+        int ret = inet_aton(ip, &server_addr.sin_addr);
         assert(-1 != ret);
 
         ret = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr));
